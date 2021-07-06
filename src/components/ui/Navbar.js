66d5dc1dfff55a597a028d900078b6dbdc57cc23
Yuki xD <pxnditxyr@gmail..com>
2021-07-06 14:44:59 -0400

    Added Component ui Navbar

---
+++ src/components/ui/Navbar.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+export const Navbar = () => {
+
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <Link
+                className="navbar-brand"
+                to="/"
+            >
+                Asociaciones
+            </Link>
+
+            <div className="navbar-collapse">
+                <div className="navbar-nav">
+
+                    <NavLink
+                        activeClassName="active"
+                        className="nav-item nav-link"
+                        exact
+                        to="/strength"
+                    >
+                        Strength
+                    </NavLink>
+
+                    <NavLink
+                        activeClassName="active"
+                        className="nav-item nav-link"
+                        exact
+                        to="/agility"
+                    >
+                        Agility
+                    </NavLink>
+                    <NavLink
+                        activeClassName="active"
+                        className="nav-item nav-link"
+                        exact
+                        to="/intelligence"
+                    >
+                        Intelligence
+                    </NavLink>
+                </div>
+            </div>
+
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                <ul className="navbar-nav ml-auto">
+                    <NavLink 
+                        activeClassName="active"
+                        className="nav-item nav-link" 
+                        exact
+                        to="/login"
+                    >
+                        Logout
+                    </NavLink>
+                </ul>
+            </div>
+        </nav>
+    );
+
+};
